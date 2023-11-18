0beeda913e50ce014a229e8c5fcde415e077e95a
IIIedry <98078659+IIIedry@users.noreply.github.com>
2023-11-18 11:38:11 +0600

    cupecakescreen class filling

--- Navigate/app/src/main/java/com/example/navigatebetweenscreenswithcompose/CupcakeScreen.kt
+++ Navigate/app/src/main/java/com/example/navigatebetweenscreenswithcompose/CupcakeScreen.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -17,26 +16,28 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
-import javax.sql.DataSource
-
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
-import com.example.cupcake.data.DataSource
-import com.example.cupcake.data.OrderUiState
-import com.example.cupcake.ui.OrderSummaryScreen
-import com.example.cupcake.ui.OrderViewModel
-import com.example.cupcake.ui.SelectOptionScreen
-import com.example.cupcake.ui.StartOrderScreen
+import com.example.navigatebetweenscreenswithcompose.R
+import com.example.cupcake.R
+import com.example.navigatebetweenscreenswithcompose.data.DataSource
+import com.example.navigatebetweenscreenswithcompose.data.OrderUiState
+import com.example.navigatebetweenscreenswithcompose.ui.OrderSummaryScreen
+import com.example.navigatebetweenscreenswithcompose.ui.OrderViewModel
+import com.example.navigatebetweenscreenswithcompose.ui.SelectOptionScreen
+import com.example.navigatebetweenscreenswithcompose.ui.StartOrderScreen
+import javax.sql.DataSource
+
 
 enum class CupcakeScreen(@StringRes val title: Int) {
     Start(title = R.string.app_name),
@@ -45,7 +46,6 @@ enum class CupcakeScreen(@StringRes val title: Int) {
     Summary(title = R.string.order_summary)
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CupcakeAppBar(
     currentScreen: CupcakeScreen,
@@ -72,7 +72,6 @@ fun CupcakeAppBar(
     )
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CupcakeApp(
     viewModel: OrderViewModel = viewModel(),
@@ -154,6 +153,7 @@ fun CupcakeApp(
         }
     }
 }
+
 
 private fun cancelOrderAndNavigateToStart(
     viewModel: OrderViewModel,
--- Navigate/app/src/main/java/com/example/navigatebetweenscreenswithcompose/MainActivity.kt
+++ Navigate/app/src/main/java/com/example/navigatebetweenscreenswithcompose/MainActivity.kt
@@ -3,13 +3,6 @@ package com.example.navigatebetweenscreenswithcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.view.WindowCompat
 import com.example.navigatebetweenscreenswithcompose.ui.theme.NavigateBetweenScreensWithComposeTheme
 
