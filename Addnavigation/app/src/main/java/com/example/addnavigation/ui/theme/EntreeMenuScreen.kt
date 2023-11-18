b90072e2bd2c31bd2b7d90d7f043f098a7ffac29
IIIedry <98078659+IIIedry@users.noreply.github.com>
2023-11-18 13:06:26 +0600

    preview filling

--- Addnavigation/app/src/main/java/com/example/addnavigation/ui/theme/EntreeMenuScreen.kt
+++ Addnavigation/app/src/main/java/com/example/addnavigation/ui/theme/EntreeMenuScreen.kt
@@ -1,7 +1,14 @@
 package com.example.addnavigation.ui.theme
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.addnavigation.R
+import com.example.addnavigation.data.DataSource
 import com.example.addnavigation.model.MenuItem
 import com.example.addnavigation.ui.BaseMenuScreen
 
@@ -19,5 +26,18 @@ fun EntreeMenuScreen(
         onNextButtonClicked = onNextButtonClicked,
         onSelectionChanged = onSelectionChanged as (MenuItem) -> Unit,
         modifier = modifier
+    )
+}
+@Preview
+@Composable
+fun EntreeMenuPreview(){
+    EntreeMenuScreen(
+        options = DataSource.entreeMenuItems,
+        onCancelButtonClicked = {},
+        onNextButtonClicked = {},
+        onSelectionChanged = {},
+        modifier = Modifier
+            .padding(dimensionResource(R.dimen.padding_medium))
+            .verticalScroll(rememberScrollState())
     )
 }
