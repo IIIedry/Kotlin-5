74401218d17534ae7d4fbab9d68c4547d74959ca
IIIedry <98078659+IIIedry@users.noreply.github.com>
2023-11-18 10:42:22 +0600

    capcakescreen class added

---
+++ Navigate/app/src/main/java/com/example/navigatebetweenscreenswithcompose/CupcakeScreen.kt
@@ -0,0 +1,10 @@
+package com.example.navigatebetweenscreenswithcompose
+
+import androidx.annotation.StringRes
+
+enum class CupcakeScreen(@StringRes val title: Int) {
+    Start(title = R.string.app_name),
+    Flavor(title = R.string.choose_flavor),
+    Pickup(title = R.string.choose_pickup_date),
+    Summary(title = R.string.order_summary)
+}
