151ffa823f9ab7b2622a79ab53c113594165445a
IIIedry <98078659+IIIedry@users.noreply.github.com>
2023-11-18 13:04:05 +0600

    create EntreeMenuScreen class inlclude function

---
+++ Addnavigation/app/src/main/java/com/example/addnavigation/ui/theme/EntreeMenuScreen.kt
@@ -0,0 +1,21 @@
+package com.example.addnavigation.ui.theme
+
+import androidx.compose.runtime.Composable
+import com.example.addnavigation.ui.BaseMenuScreen
+
+@Composable
+fun EntreeMenuScreen(
+    options: List<EntreeItem>,
+    onCancelButtonClicked: () -> Unit,
+    onNextButtonClicked: () -> Unit,
+    onSelectionChanged: (EntreeItem) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    BaseMenuScreen(
+        options = options,
+        onCancelButtonClicked = onCancelButtonClicked,
+        onNextButtonClicked = onNextButtonClicked,
+        onSelectionChanged = onSelectionChanged as (MenuItem) -> Unit,
+        modifier = modifier
+    )
+}
