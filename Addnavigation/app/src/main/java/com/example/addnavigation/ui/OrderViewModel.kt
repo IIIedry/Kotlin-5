efccf1d8267c7b80de16cb1e78c5a3c7e67c77ce
IIIedry <98078659+IIIedry@users.noreply.github.com>
2023-11-25 07:48:43 +0600

    complete update item

--- Addnavigation/app/src/main/java/com/example/addnavigation/ui/OrderViewModel.kt
+++ Addnavigation/app/src/main/java/com/example/addnavigation/ui/OrderViewModel.kt
@@ -4,8 +4,11 @@ import androidx.lifecycle.ViewModel
 import com.example.addnavigation.model.MenuItem
 import com.example.addnavigation.model.OrderUiState
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.update
 
 class OrderViewModel : ViewModel() {
+
+    private val taxRate = 0.08
 
     private val _uiState = MutableStateFlow(OrderUiState())
 
@@ -15,7 +18,22 @@ class OrderViewModel : ViewModel() {
     }
 
     private fun updateItem(newItem: MenuItem, previousItem: MenuItem?){
-
+        _uiState.update { currentState ->
+            val previousItemPrice = previousItem?.price ?: 0.0
+            // subtract previous item price in case an item of this category was already added.
+            val itemTotalPrice = currentState.itemTotalPrice - previousItemPrice + newItem.price
+            // recalculate tax
+            val tax = itemTotalPrice * taxRate
+            currentState.copy(
+                itemTotalPrice = itemTotalPrice,
+                orderTax = tax,
+                orderTotalPrice = itemTotalPrice + tax,
+                entree = if (newItem is MenuItem.EntreeItem) newItem else currentState.entree,
+                sideDish = if (newItem is MenuItem.SideDishItem) newItem else currentState.sideDish,
+                accompaniment =
+                if (newItem is MenuItem.AccompanimentItem) newItem else currentState.accompaniment
+            )
+        }
     }
 
 }
