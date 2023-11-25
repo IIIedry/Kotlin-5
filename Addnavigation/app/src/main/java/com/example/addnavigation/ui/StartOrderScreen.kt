51e5b5e7cf71794016498975d613aabb2e34d0bb
IIIedry <98078659+IIIedry@users.noreply.github.com>
2023-11-25 07:54:58 +0600

    start order file created

---
+++ Addnavigation/app/src/main/java/com/example/addnavigation/ui/StartOrderScreen.kt
@@ -0,0 +1,18 @@
+package com.example.addnavigation.ui
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+fun StartOrderScreen(
+    onStartOrderButtonClicked: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    )
+
+}
