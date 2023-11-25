2b13b28ef73a788cc97134263a0833d1f62538ac
IIIedry <98078659+IIIedry@users.noreply.github.com>
2023-11-25 07:55:41 +0600

    order file basic idea complete

--- Addnavigation/app/src/main/java/com/example/addnavigation/ui/StartOrderScreen.kt
+++ Addnavigation/app/src/main/java/com/example/addnavigation/ui/StartOrderScreen.kt
@@ -2,9 +2,17 @@ package com.example.addnavigation.ui
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.example.addnavigation.R
 
+@Composable
 fun StartOrderScreen(
     onStartOrderButtonClicked: () -> Unit,
     modifier: Modifier = Modifier
@@ -13,6 +21,12 @@ fun StartOrderScreen(
         modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
-    )
-
+    ) {
+        Button(
+            onClick = onStartOrderButtonClicked,
+            Modifier.widthIn(min = 250.dp)
+        ) {
+            Text(stringResource(R.string.start_order))
+        }
+    }
 }
