eb345886783d52c92bbcb366037b22f1ac0ea1aa
IIIedry <98078659+IIIedry@users.noreply.github.com>
2023-11-25 07:56:03 +0600

    order file preview complete

--- Addnavigation/app/src/main/java/com/example/addnavigation/ui/StartOrderScreen.kt
+++ Addnavigation/app/src/main/java/com/example/addnavigation/ui/StartOrderScreen.kt
@@ -2,13 +2,17 @@ package com.example.addnavigation.ui
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.addnavigation.R
 
@@ -29,4 +33,15 @@ fun StartOrderScreen(
             Text(stringResource(R.string.start_order))
         }
     }
+}
+
+@Preview
+@Composable
+fun StartOrderPreview(){
+    StartOrderScreen(
+        onStartOrderButtonClicked = {},
+        modifier = Modifier
+            .padding(dimensionResource(R.dimen.padding_medium))
+            .fillMaxSize()
+    )
 }
